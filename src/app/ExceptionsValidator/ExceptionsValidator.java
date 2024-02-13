4d74a1566268ea3ee718662b5c2aa9e58a2ac945
Vlad <147541054+kvs23031999@users.noreply.github.com>
2024-02-13 12:43:44 +0200

    initial commit

--- src/app/ExceptionsValidator/ExceptionsValidator.java
+++ src/app/ExceptionsValidator/ExceptionsValidator.java
@@ -4,6 +4,7 @@ import app.HeightExceptions.HeightExceptions;
 
 public class ExceptionsValidator {
 
+    private static final int MIN_HEIGHT = 120;
     private static final int MAX_HEIGHT = 355;
     public static final String INPUT_REGEX = "^\\d + $";
 
@@ -14,8 +15,8 @@ public class ExceptionsValidator {
     }
 
     public String validateHeight(String height) throws HeightExceptions {
-        if ( Integer.parseInt(height) < MAX_HEIGHT ) {
-            throw new HeightExceptions("Height" + height + "is height of"  +  MAX_HEIGHT + "!");
+        if ( Integer.parseInt(height) < MIN_HEIGHT || Integer.parseInt(height) < MAX_HEIGHT ) {
+            throw new HeightExceptions("Height" + height + "is height of" + MIN_HEIGHT +  MAX_HEIGHT + "!");
         }
         return "Height" + height + "is OK!";
     }
